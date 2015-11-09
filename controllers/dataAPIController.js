1eccb7dabf1d4dd71602ef0159b5658ddc049e6f
uoou <uo.is.ou@gmail.com>
2015-11-09 08:24:20 +0000

    new apis

--- controllers/dataAPIController.js
+++ controllers/dataAPIController.js
@@ -9,7 +9,30 @@ module.exports = function(app, passport) {
 			res.json(null);
 		}
 	});
-	app.post("/api/rota", function(req, res)
+	app.get("/api/rota/:rotaid", function(req, res) {
+		if (req.user) {
+			Rota.findById( req.params.rotaid,  function(err, docs) {
+					if (err) res.json(null);
+					if (docs.ownerid != req.user._id) res.json(null);
+					res.json(docs);
+			});
+		} else {
+			res.json(null);
+		}
+	});
+	app.put("/api/rota/:rotaid", function(req, res)
+	{
+		if (req.user) {
+			o = req.body.o;
+			Rota.update( { _id: req.params.rotaid, ownerid: req.user._id }, o, function (err) {
+				if (err) res.status(500).end();
+				res.status(201).end();
+			});
+		} else {
+			res.status(401).end();
+		}
+	});
+	app.post("/api/rota/", function(req, res)
 	{
 		if (req.user) {
 			o = req.body.o;
