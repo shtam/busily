8613ed0aad5b40ef60c94654fccff4709708de14
uoou <uo.is.ou@gmail.com>
2015-11-16 20:10:06 +0000

    mend api

--- controllers/dataAPIController.js
+++ controllers/dataAPIController.js
@@ -1,61 +1,39 @@
 module.exports = function(app, passport) {
 	app.get("/api/rota", function(req, res) {
-		if (req.user) {
-			Rota.find({ ownerid: req.user._id }).exec(function(err, docs) {
-					if (err) res.json(null);
-					res.json(docs);
-			});
-		} else {
-			res.json(null);
-		}
+		Rota.find({ ownerid: req.user._id }).exec(function(err, docs) {
+				if (err) res.json(null);
+				res.json(docs);
+		});
 	});
 	app.get("/api/rota/:rotaid", function(req, res) {
-		if (req.user) {
-			Rota.findById( req.params.rotaid,  function(err, docs) {
-					if (err) res.json(null);
-					if (docs.ownerid != req.user._id) res.json(null);
-					res.json(docs);
-			});
-		} else {
-			res.json(null);
-		}
+		Rota.findById( req.params.rotaid,  function(err, docs) {
+				if (err) res.json(null);
+				res.json(docs);
+		});
 	});
 	app.put("/api/rota/:rotaid", function(req, res)
 	{
-		if (req.user) {
-			o = req.body.o;
-			Rota.update( { _id: req.params.rotaid, ownerid: req.user._id }, o, function (err) {
-				if (err) res.status(500).end();
-				res.status(201).end();
-			});
-		} else {
-			res.status(401).end();
-		}
+		o = req.body.o;
+		Rota.update( { _id: req.params.rotaid }, o, function (err) {
+			if (err) res.status(500).end();
+			res.status(201).end();
+		});
 	});
 	app.post("/api/rota/delete/:rotaid", function(req, res)
 	{
-		if (req.user) {
-			Rota.remove( { _id: req.params.rotaid, ownerid: req.user._id }, function (err) {
-				if (err) res.status(500).end();
-				res.status(201).end();
-			});
-		} else {
-			res.status(401).end();
-		}
+		Rota.remove( { _id: req.params.rotaid }, function (err) {
+			if (err) res.status(500).end();
+			res.status(201).end();
+		});
 	});
 	app.post("/api/rota/", function(req, res)
 	{
-		if (req.user) {
-			o = req.body.o;
-			o.ownerid = req.user._id;
-			var newrota = new Rota(o);
-			newrota.save(function (err) {
-				if (err) res.status(500).end();
-				res.status(201).end();
-			});
-		} else {
-			res.status(401).end();
-		}
+		o = req.body.o;
+		var newrota = new Rota(o);
+		newrota.save(function (err) {
+			if (err) res.status(500).end();
+			res.status(201).end();
+		});
 	});
 	app.get("/api/userrota/:userid", function(req, res) {
 		if (req.params.userid) {
